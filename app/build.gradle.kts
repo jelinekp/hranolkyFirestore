d4b307cd68be310a88c3a8ee4b571f530a199abc
jelinekp <jelinekp6@gmail.com>
2025-09-06 23:38:40 +0200

    app version 2.1 ready to ship

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "eu.jelinek.hranolky"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0"
+        versionCode = 5
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
